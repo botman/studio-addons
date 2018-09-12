c34e49b1d57a1a327ce15b9ac3026217f8f44a7c
Rachid Laasri <36804104+rashidlaasri@users.noreply.github.com>
2018-09-12 15:13:10 +0100

    Add botman:cache:clear command (#26)

---
+++ src/Console/Commands/BotManCacheClear.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace BotMan\Studio\Console\Commands;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Console\GeneratorCommand;
+
+class BotManCacheClear extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'botman:cache:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove all cached conversations.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param \Illuminate\Filesystem\Filesystem $files
+     * @return mixed
+     */
+    public function handle(Filesystem $files)
+    {
+        $cacheFolder = storage_path('botman');
+
+        if ($files->exists($cacheFolder)) {
+            $files->cleanDirectory($cacheFolder);
+        }
+
+        $this->info('BotMan cache cleared!');
+    }
+}
