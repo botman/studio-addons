027ff2d03d9082cef2f7d74f791096144ca15b82
QWp6t <QWp6t@users.noreply.github.com>
2020-09-04 09:25:11 +0800

    Fix process parameters.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Composer.php
+++ src/Composer.php
@@ -2,6 +2,7 @@
 
 namespace BotMan\Studio;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Composer as BaseComposer;
 
 class Composer extends BaseComposer
@@ -14,12 +15,30 @@ class Composer extends BaseComposer
      */
     public function install($package, callable $callback)
     {
-        $process = $this->getProcess();
-
-        $process->setCommandLine(trim($this->findComposer().' require '.$package));
+        $process = $this->installationCommandProcess($package);
 
         $process->run($callback);
 
         return $process->isSuccessful();
+    }
+
+    /**
+     * Get installation command for process.
+     *
+     * @param  string $package
+     * @return \Symfony\Component\Process\Process
+     */
+    protected function installationCommandProcess($package)
+    {
+        if (version_compare(Application::version, '5.8.0', '<')) {
+            $process = $this->getProcess();
+
+            return $process->setCommandLine(trim($this->findComposer().' require '.$package));
+        }
+
+        $command = $this->findComposer();
+        array_push($command, 'require', $package);
+
+        return $this->getProcess($command);
     }
 }
